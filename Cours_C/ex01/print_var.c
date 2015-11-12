e6acdca01c687ae1a9d92da400720bb1ddd78626
Trofimiuk Pawel <trofimiuk.pawel@gmail.com>
2015-11-12 04:28:57 +0100

    Premier exo

---
+++ Cours_C/ex01/print_var.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int	main()
+{
+	 float a;
+	 float b;
+
+	 printf("a = ");
+	 scanf("%f", &a);
+	 printf("b = ");
+	 scanf("%f", &b);
+	 printf("Le produit de l'operation est %f", a*b);
+	 return (a*b);
+}
